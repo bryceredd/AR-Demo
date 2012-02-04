d8047c9e5ca7922b652bd026b89307adfc3bf2ca
Guy Harding <guy@verisagellc.com>
2012-02-04 01:53:34 -0700

    Initial radar screen

---
+++ ARTest/ARLRadarView.h
@@ -0,0 +1,13 @@
+//
+//  ARLRadarView.h
+//  ARTest
+//
+//  Created by Guy Harding on 2/4/12.
+//  Copyright (c) 2012 Itv. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ARLRadarView : NSObject
+
+@end
---
+++ ARTest/ARLRadarView.m
@@ -0,0 +1,13 @@
+//
+//  ARLRadarView.m
+//  ARTest
+//
+//  Created by Guy Harding on 2/4/12.
+//  Copyright (c) 2012 Itv. All rights reserved.
+//
+
+#import "ARLRadarView.h"
+
+@implementation ARLRadarView
+
+@end
